56c70c53b4cd0d458eaf04ec55031d4ab4004019
Wuddy <289764783@qq.com>
2022-11-18 18:45:00 +0800

    first commit

---
+++ src/main/java/com/wzq/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzq.git;
+
+/**
+ * @author wudy
+ * @create 2022-11-18 18:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
